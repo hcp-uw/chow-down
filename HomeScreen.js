acd4e510b235f1272bf5d815108afc797ea10295
mygcho <109271804+mygcho@users.noreply.github.com>
2022-08-09 16:42:11 -0700

    search bar front end

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+// to install the icon, use commands in this order:
+// i --save @fortawesome/react-native-fontawesome @fortawesome/fontawesome-svg-core react-native-svg
+// i --save @fortawesome/free-solid-svg-icons
+// i --save @fortawesome/free-brands-svg-icons
+// i --save @fortawesome/free-regular-svg-icons
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
 
+// TODO: figure out how to make the search bar a separate component, and connect to HomeScreen.js
 
 /**
  * Home Screen display for App.js
@@ -11,6 +19,16 @@ function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
             <Text>Home Screen</Text>
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                paddingHorizontal: 20,
+            }}>
+                <View style={styles.searchInputContainer}>
+                    <FontAwesomeIcon icon={ faMagnifyingGlass } />
+                    <TextInput style={styles.textSpace} placeholder="Search for..."/>
+                </View>
+            </View>
 
             <View style={styles.smallSpace} />
 
@@ -36,6 +54,18 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    searchInputContainer: {
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        borderRadius: 100,
+    },
+    textSpace: {
+        marginLeft: 10,
     },
     /** Adds small space between objects for readability purposes */
     smallSpace: {
