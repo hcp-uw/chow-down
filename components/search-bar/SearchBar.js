242cc9d0f2eaae40de6dda05871a8e9e055cc6b4
kevkevin999 <94778542+kevkevin999@users.noreply.github.com>
2022-08-21 22:03:10 -0700

    minor style changes

--- components/search-bar/SearchBar.js
+++ components/search-bar/SearchBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StyleSheet, View, TextInput, Text} from 'react-native';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
 import JSONDATA from './MOCK_DATA.json';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 // function SearchBar({ placeholder, data }) {
@@ -17,51 +17,49 @@ import {useState} from 'react';
 
 // event.target.value
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState('')
-    return(
-       <View className="search">
-          <TextInput
-             type="text"
-             placeholder="Search..."
-             onChangeText={setSearchTerm} //</View>=>
-                // this.setState({setSearchTerm: event})
-                // {setSearchTerm(this.setState({event}))  
-             >
-          </TextInput>
-          {JSONDATA.filter((val)=> {
-             if (searchTerm == "") {
-                return val
-             } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
-                return val
-             }
-          }).map((val, key)=> { //Filter function here
-             return (
-             <View className="user" key={key}>
-                <Text> {val.first_name} </Text>
-             </View>
-             );
-          })}
-       </View>
-    );
-  }
-  
+   const [searchTerm, setSearchTerm] = useState('')
+   return (
+      <View className="search">
+         <TextInput
+            type="text"
+            placeholder="Search..."
+            onChangeText={setSearchTerm}
+         >
+         </TextInput>
+         {JSONDATA.filter((val) => {
+            if (searchTerm == "") {
+               return val
+            } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+               return val
+            }
+         }).map((val, key) => { //Filter function here
+            return (
+               <View className="user" key={key}>
+                  <Text> {val.first_name} </Text>
+               </View>
+            );
+         })}
+      </View>
+   );
+}
+
 
 const styles = StyleSheet.create({
    container: {
       backgroundColor: 'yellow',
    },
    searchInputContainer: {
-        height: 50,
-        backgroundColor: '#FFFFFF',
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        borderRadius: 100,
-    },
-    textSpace: {
-        marginLeft: 10,
-    },
+      height: 50,
+      backgroundColor: '#FFFFFF',
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      borderRadius: 100,
+   },
+   textSpace: {
+      marginLeft: 10,
+   },
 })
 
 export default SearchBar;
