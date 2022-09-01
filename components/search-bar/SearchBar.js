e08f0a75809867290f070157a611d1642759f676
jonathanbustamanteosorio <jbusta@uw.edu>
2022-08-31 21:18:48 -0700

    no change

--- components/search-bar/SearchBar.js
+++ components/search-bar/SearchBar.js
@@ -3,6 +3,7 @@ import { StyleSheet, Keyboard, TouchableOpacity, KeyboardAvoidingView, SafeAreaV
 import JSONDATA from './MOCK_DATA.json';
 import { useState } from 'react';
 import CustomRestaurantButton from '../custom-buttons/CustomRestaurantButton';
+import { ScrollView } from 'react-native-gesture-handler';
 
 // NOTE: to test that Keyboard appearance works on SearchPage.js,
 // you have to use Expo Go on a separate device. Does not show up on emulator.
@@ -10,7 +11,8 @@ const SearchBar = ({ navigation }) => {
    const [searchTerm, setSearchTerm] = useState('')
 
    return (
-      <SafeAreaView className="search" >
+      // <ScrollView>
+      <ScrollView className="search" >
          <KeyboardAvoidingView style={styles.searchInputContainer}>
             <TouchableOpacity>
                <TextInput style={styles.textInputSpace}
@@ -22,13 +24,17 @@ const SearchBar = ({ navigation }) => {
                </TextInput>
             </TouchableOpacity>
          </KeyboardAvoidingView>
+         {/* <ScrollView> */}
          {JSONDATA.filter((val) => {
             if (searchTerm == "") {
                return val
             } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
                return val
             }
-         }).map((val, key) => { //Filter function and style for search results here
+         }).map((val, key) => 
+         
+         { //Filter function and style for search results here
+            
             return ( // TODO: maybe --> Switch .map to something more compatiable for React Native- FlatList
                <CustomRestaurantButton
                   className="user"
@@ -42,9 +48,11 @@ const SearchBar = ({ navigation }) => {
                   onPress={() => navigation.navigate('BusinessPage', { name: val.first_name + ' ' + val.last_name })}
                />
             )
+         
          }
+         
          )}
-      </SafeAreaView>
+      </ScrollView>
    );
 }
 
