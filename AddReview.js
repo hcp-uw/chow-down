32b16955ae4cc7778cae1ae6fa8d63c0d84cd03d
kevkevin999 <94778542+kevkevin999@users.noreply.github.com>
2022-09-12 23:25:49 -0700

    still debugging addReview

--- AddReview.js
+++ AddReview.js
@@ -4,17 +4,32 @@ import * as FileSystem from 'expo-file-system';
 import JSONDATA from './components/search-bar/MOCK_DATA.json'
 
 const AddReview = () => {
-    // Storing the JSON format data in myObject
-    try {
-        const restaurantObject = FileSystem.readAsStringAsync(JSONDATA);
-        console.log(restaurantObject);
-        // const restaurantObject = JSON.parse(data);
-        restaurantObject.array.forEach(element => {
-            element.restaurantObject.push('hello');
-        });
-    } catch (error) {
-        console.log(error)
+    // Attempting to use FileSystem (from expo-file-system) to read JSONDATA
+    async function readReviews() {
+        try {
+            console.log("entered function")
+            // console.log(JSONDATA)
+            const data = FileSystem.readAsStringAsync(JSONDATA);
+            console.log("reading data");
+            console.log(data);
+            // currently "data" is this: Promise {
+            // "_U": 0,
+            // "_V": 0,
+            // "_W": null,
+            // "_X": null,
+            // }
+            const dataAwait = await data; // causing bugs
+            // console.log("data awaited")
+            // const restaurantObject = JSON.parse(data);
+            // restaurantObject.array.forEach(element => {
+            //     console.log("working");
+            // });
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+
 
     // restaurantObject.array.forEach(element => {
     //     element.restaurantObject.push('hello');
@@ -29,7 +44,8 @@ const AddReview = () => {
 
     return (
         <View>
-            <Text> Hello </Text>
+            <Text> {readReviews()} </Text>
+
         </View>
     )
 }
