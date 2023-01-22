95eb55733abec97e3e6431a85c2a2e7276120c05
nickhitos <nickhit3802@gmail.com>
2023-01-22 15:17:19 -0800

    Connected app to Firebase

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Text, View } from 'react-native';
 import BottomTabs from './navigators/BottomTabs';
 import 'react-native-gesture-handler';
-
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, set } from 'firebase/database';
 
 // 8/11/22: npm install @react-navigation/bottom-tabs
 
@@ -12,18 +13,33 @@ import 'react-native-gesture-handler';
  * @returns Display for mobile device
  */
 export default function App() {
+  const firebaseConfig = {
+    // INSERT CONFIG HERE
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  function storeHighScore(userId, score) {
+    const db = getDatabase();
+    const reference = ref(db, 'users/' + userId);
+    set(reference, {
+      highscore: score,
+    });
+  }
+
   return (
     <View style={styles.container}>
-        <StatusBar style="auto" />
-        <BottomTabs/>
+      <Button title='Send Data' onPress={storeHighScore("user1", 500)}></Button>
+      <StatusBar style="auto" />
+      <BottomTabs />
     </View>
   );
-} 
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFF4BE',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF4BE',
+  }
 })
 
